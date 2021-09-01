5fbbe41ea65c9b8805947518fe49a112c9bac092
Lucas Santos <lusantosatl@gmail.com>
2021-09-01 10:25:53 -0400

    select movie functionality

--- script.js
+++ script.js
@@ -1,29 +1,41 @@
 const container = document.querySelector('.container')// will select just one node element
-const seat = document.querySelectorAll('.row .seat:not(.occupied)') //will select multiple nodes and arrange them in an array. you can then use array methods on them
+const seats = document.querySelectorAll('.row .seat:not(.occupied)') //will select multiple nodes and arrange them in an array. you can then use array methods on them
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 
 let ticketPrice = +movieSelect.value;
 
+
+// Save selected movie index and price
+function setMovieData(movieIndex, moviePrice) {
+    localStorage.setItem('selectedMovieIndex', movieIndex);
+    localStorage.setItem('selectedMoviePrice', moviePrice);
+}
+
 // update total and count
 function updateSelectedCount() {
     const selectedSeats = document.querySelectorAll('.row .seat.selected');
     
+    const seatsIndex = [...selectedSeats].map((seat) => [...seats].indexOf(seat));
+
+    localStorage.setsItem('selectedSeats', JSON.stringify(seatsIndex));
+
     const selectedSeatsCount = selectedSeats.length;
     
     count.innerText = selectedSeatsCount;
     total.innerText = selectedSeatsCount * ticketPrice;
-}
+} 
 
 // Movie select event
 movieSelect.addEventListener('change',(e) => {
     ticketPrice = +e.target.value;
+    setMovieData(e.target.selectedIndex, e.target.value);
     updateSelectedCount();
 })
 
 
-// Seat click event
+// Seat click event 
 container.addEventListener('click', (e) => {
     if(e.target.classList.contains('seat') && 
     !e.target.classList.contains('occupied')) {
